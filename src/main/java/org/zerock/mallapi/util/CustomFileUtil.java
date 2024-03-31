739f5f776d05d7455bd80654c47255d72ad36937
shvyeon <gustjsgp8569@naver.com>
2024-03-31 19:34:03 +0900

    파일 업로드 기능 추가 - thumbnail 설정 라이브러리 추가 - 파일 경로 설정 추가 - 파일 업로드 / 삭제 기능 추가

---
+++ src/main/java/org/zerock/mallapi/util/CustomFileUtil.java
@@ -0,0 +1,126 @@
+package org.zerock.mallapi.util;
+
+import jakarta.annotation.PostConstruct;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import net.coobird.thumbnailator.Thumbnails;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.core.io.Resource;
+
+import javax.imageio.IIOException;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+@Component
+@Log4j2
+@RequiredArgsConstructor
+public class CustomFileUtil {
+    @Value("${upload.file.path}")
+    private String uploadPath;
+
+    @PostConstruct
+    public void init(){
+        File tempFolder = new File(uploadPath);
+
+        if(!tempFolder.exists()){
+            tempFolder.mkdir();
+        }
+
+        uploadPath = tempFolder.getAbsolutePath();
+
+        log.info("----------------------------------------");
+        log.info(uploadPath);
+    }
+
+    // 파일 업로드
+    public List<String> saveFiles(List<MultipartFile> files) throws RuntimeException{
+        if(files == null || files.size() == 0){
+            return List.of();
+        }
+
+        List<String> uploadNames = new ArrayList<>();
+
+        for(MultipartFile multipartFile : files){
+            String savedName = UUID.randomUUID().toString() + "_" + multipartFile.getOriginalFilename();
+
+            Path savePath = Paths.get(uploadPath, savedName);
+
+            try{
+                Files.copy(multipartFile.getInputStream(),savePath);
+                String contentType = multipartFile.getContentType();
+
+                // 이미지 여부 확인
+                if(contentType !=null && contentType.startsWith("image")){
+                    Path thumbnailPath = Paths.get(uploadPath, "s_" + savedName);
+                    Thumbnails.of(savePath.toFile())
+                            .size(200,200)
+                            .toFile(thumbnailPath.toFile());
+                }
+
+                uploadNames.add(savedName);
+            }catch (IOException e){
+                throw new RuntimeException((e.getMessage()));
+            }
+        }
+
+        return uploadNames;
+    }
+
+    // 파일 가져오기
+    public ResponseEntity<Resource> getFile(String fileName){
+        Resource resource = new FileSystemResource(uploadPath + File.separator + fileName);
+
+        if(!resource.isReadable()){
+            resource = new FileSystemResource(uploadPath + File.separator + "default.jpeg");
+        }
+
+        HttpHeaders headers = new HttpHeaders();
+
+        try{
+            headers.add("Content-Type", Files.probeContentType(resource.getFile().toPath()));
+        }catch (Exception e){
+            return ResponseEntity.internalServerError().build();
+        }
+
+        return ResponseEntity.ok().headers(headers).body(resource);
+    }
+
+    // 파일 삭제
+    public void deleteFiles(List<String> fileNames){
+        if(fileNames == null || fileNames.isEmpty()){
+            return;
+        }
+
+        fileNames.forEach(fileName -> {
+            // 썸네일이 있는지 확인하고 삭제
+            String thumbnailFileName = "s_" + fileName;
+            Path thumbnatilPath = Paths.get(uploadPath, thumbnailFileName);
+            Path filePath = Paths.get(uploadPath, fileName);
+
+            try{
+                Files.deleteIfExists(filePath);
+                Files.deleteIfExists(thumbnatilPath);
+
+            }catch (IOException e){
+                throw new RuntimeException(e.getMessage());
+            }
+        });
+    }
+
+
+
+
+
+}
